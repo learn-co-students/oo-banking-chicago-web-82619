aac2c728ce45590086cb2d1e90a1fcb54ef17f7e
github username <github email address>
2019-08-29 14:25:58 -0500

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,39 @@
 class BankAccount
 
+    attr_reader :name
+    attr_accessor :balance, :status
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+        @@all << self
+    end
+
+    def deposit(amt)
+        @balance = @balance + amt 
+    end
+
+    def withdrawal(amt)
+        @balance = @balance - amt
+    end
+
+    def display_balance
+        "Your balance is $#{@balance}."
+    end
+
+    def valid?
+        if (self.balance > 0 && self.status == "open")
+            true
+        else 
+            false
+        end
+    end
+
+    def close_account
+        self.status = "closed"
+    end
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,45 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    if (sender.valid? && receiver.valid?)
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+    if valid? && sender.balance > amount && @status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      @status = "complete"
+    else
+      rollback
+    end
+  end
+
+  def reverse_transfer
+    if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed"
+    else
+      rollback
+    end
+  end
+
+  def rollback
+    @status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+
 end
